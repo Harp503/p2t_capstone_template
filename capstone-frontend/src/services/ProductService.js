856b9f5199210142072310e0d0b7e0a345bc8d1c
Justin <harpfam18@outlook.com>
2025-08-31 21:06:24 -0400

    removing config for deployment

--- capstone-frontend/src/services/ProductService.js
+++ capstone-frontend/src/services/ProductService.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-import { API_BASE_URL } from '.config';
+
+const baseUrl = 'https://harp503-capstone.onrender.com/';
 
 export async function getProducts() {
-    const { data } = await axios.get(`${API_BASE_URL}products`);
+    const { data } = await axios.get(`${baseUrl}products`);
     return data;
 }
 
 // This function retrieves a product by its ID and has been created for you, but you will need to import it in your components as needed.
 export async function getProductById(id) {
-    const { data } = await axios.get(`${API_BASE_URL}products/${id}`);
+    const { data } = await axios.get(`${baseUrl}products/${id}`);
     return data;
 }
