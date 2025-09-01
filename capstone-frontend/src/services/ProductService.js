b1fc211c316632db123f7a28edb1b0077f8d69fd
Justin <harpfam18@outlook.com>
2025-08-31 20:57:27 -0400

    Update ProductService.js

--- capstone-frontend/src/services/ProductService.js
+++ capstone-frontend/src/services/ProductService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_BASE_URL } from './config';
+import { API_BASE_URL } from '.config';
 
 export async function getProducts() {
     const { data } = await axios.get(`${API_BASE_URL}products`);
